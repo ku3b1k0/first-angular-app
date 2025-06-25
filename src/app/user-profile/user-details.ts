0255f80f5063de51664a6d80efb701940129442a
ku3b1k0 <admin@kuebiko.in>
2025-06-25 19:17:32 +0530

    chore(components): simple interpolation with if conditional and model

---
+++ src/app/user-profile/user-details.ts
@@ -0,0 +1,8 @@
+export interface UserDetails {
+  userName: string;
+  userTitle: string;
+  myTitle: string;
+  userMembership: string;
+  description: string;
+  profileImage: string;
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserDetails } from './user-details';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.component.html',
 })
 export class UserProfileComponent {
-  myTitle = 'User Profile';
-  description =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  profileImage = '/assets/images/jake-nackos-IF9TK5Uy-KI-unsplash.jpg';
+  currentUser: UserDetails = {
+    userName: 'Jane Doe',
+    userTitle: 'Software Engineer',
+    myTitle: 'User Profile',
+    userMembership: 'gold',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    profileImage: '/assets/images/jake-nackos-IF9TK5Uy-KI-unsplash.jpg',
+  };
 }
